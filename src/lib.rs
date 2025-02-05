64e08a99800ad52fe11063a54521fb8bc9bedf01
Pierre-Henri <phbasic@gmail.com>
2025-02-05 09:08:53 +0100

    fix: added Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![forbid(unsafe_code)]
 #![warn(rust_2018_idioms)]
 #![warn(clippy::all)]
+#![deny(clippy::unwrap_used)]
+// False positive with None
+#![allow(non_snake_case)]
 
 pub mod aof;
 pub mod command;
